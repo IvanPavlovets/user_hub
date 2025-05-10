16f5bc2724c0aeb1b3d09d79862f8cfcdda96b48
IvanPavlovets <ivan150287@gmail.com>
2025-05-10 19:03:45 +0700

    init commit

---
+++ services/userapi/src/main/java/com/test/userapi/UserapiApplication.java
@@ -0,0 +1,16 @@
+package com.test.userapi;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class UserapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserapiApplication.class, args);
+		log.info("Go to -> :{}", "http://localhost:8080");
+	}
+
+}
